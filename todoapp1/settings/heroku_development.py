4263ec6e995f1cd3a03b454bc977b8eaedf0494a
nrabe <nrabe2@gmail.com>
2015-02-27 12:17:50 -0300

    testing auto-deploy github-heroku

--- todoapp1/settings/heroku_development.py
+++ todoapp1/settings/heroku_development.py
@@ -4,6 +4,6 @@ DEBUG = False
 TEMPLATE_DEBUG = True
 
 # in DEV, enable the authentication middleware
-BASIC_WWW_AUTHENTICATION = True
-if BASIC_WWW_AUTHENTICATION:
-    MIDDLEWARE_CLASSES += ('todoapp1.middleware.BasicAuthenticationMiddleware',)
+#BASIC_WWW_AUTHENTICATION = True
+#if BASIC_WWW_AUTHENTICATION:
+#    MIDDLEWARE_CLASSES += ('todoapp1.middleware.BasicAuthenticationMiddleware',)
