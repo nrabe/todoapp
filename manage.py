e538cc0b9f95af6635642ea324ef4b6f7830e729
nrabe <nrabe2@gmail.com>
2015-02-27 17:01:59 -0300

    testing auto-deploy github-heroku

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import logging
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tablenow.settings." + os.environ.get("ENVIRONMENT", "MUST-SET-ENVIRONMENT-VARIABLE-SEE-README"))
+logging.warn("(manage.py) DJANGO_SETTINGS_MODULE=%s %r", os.environ.get("DJANGO_SETTINGS_MODULE"), sys.argv)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoapp1.settings." + (os.environ.get("ENVIRONMENT") or "MUST-SET-ENVIRONMENT-VARIABLE-SEE-README"))
-    import logging; logging.warn("(manage.py) DJANGO_SETTINGS_MODULE=%s", os.environ.get("DJANGO_SETTINGS_MODULE"))
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
